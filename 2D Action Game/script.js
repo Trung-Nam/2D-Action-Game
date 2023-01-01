a0341d706f8c69d886faf2cdf3d8f4c3a8d4886d
Trung Nam <trungnam.work@gmail.com>
2023-01-01 19:15:20 +0700

    OOP with Js
    Object Oriented programming with JavaScript

--- 2D Action Game/script.js
+++ 2D Action Game/script.js
@@ -4,4 +4,34 @@ window.addEventListener('load', function(){
     const ctx = canvas.getContext('2d');
     canvas.width = 500;
     canvas.height = 500;
+
+
+    class InputHandler {
+        
+    }
+    class Projectitle {
+
+    }
+    class Particle {
+
+    }
+    class Player {
+
+    }
+    class Enemy {
+
+    }
+
+    class Layer {
+
+    }
+    class Background {
+
+    }
+    class UI {
+
+    }
+    class Game {
+        
+    }
 });
