18fcd1925a7ac51e7ea8bd38161ff4063afb4533
Trung Nam <trungnam.work@gmail.com>
2023-01-02 23:15:00 +0700

    Drawing projectiles as images

--- 2D Action Game/assets/js/script.js
+++ 2D Action Game/assets/js/script.js
@@ -35,15 +35,15 @@ window.addEventListener('load', function () {
             this.width = 10;
             this.height = 3;
             this.speed = 3;
-            this.markedForDeletion = false;      
+            this.markedForDeletion = false;    
+            this.image = document.getElementById('projectile');  
         }
         update() {
             this.x += this.speed;
             if(this.x > this.game.width * 0.8) this.markedForDeletion = true;
         }
         draw(context) {
-            context.fillStyle = 'yellow';
-            context.fillRect(this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.x, this.y);
         }
     }
     class Particle {
